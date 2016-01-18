4e773d5014ec7f3c39caa6fddcfcf77be50c6ed1
korneliusz <helluinster@gmail.com>
2016-01-18 22:33:12 +0100

    Make the viewbox variable. Extract transforms to functions.

--- d3/radial-control/main.js
+++ d3/radial-control/main.js
@@ -2,12 +2,32 @@ var svg = d3.select("#circle");
 
 var tau = 2 * Math.PI;
 
-svg.attr('viewBox', '0 0 100 100');
+var viewBoxSize = 120;
+var viewBoxCenter = viewBoxSize / 2;
+
+svg.attr('viewBox', '0 0 ' + viewBoxSize + ' ' + viewBoxSize);
+
+
+var svgSize = svg.node().getBoundingClientRect();
+
+// Rotation - rotate g's in CSS
 
 function range(start, n) {
     return d3.range(n).map(function(i) {
         return start + i;
     });
+}
+
+function rotateTranslate(deg) {
+    return 'rotate(' + deg + ', ' + viewBoxCenter + ', ' + viewBoxCenter + ') translate(' + viewBoxCenter + ', ' + viewBoxCenter + ')';
+}
+
+function translateRotate(deg) {
+    return transCenter() + 'rotate(' + deg + ', ' + viewBoxCenter + ', ' + viewBoxCenter + ')';
+}
+
+function transCenter() {
+    return 'translate(' + viewBoxCenter + ', ' + viewBoxCenter + ') ';
 }
 
 function arc(radius, width, style) {
@@ -42,6 +62,17 @@ function arcTween(transition, newAngle, width, arc) {
     });
 }
 
+function rotateTween(transition, newAngle, translation) {
+    transition.attrTween('transform', function(d) {
+        var interpolate = d3.interpolate(d.rotation || 0, newAngle % 361);
+
+        return function(t) {
+            d.rotation = interpolate(t);
+            return 'translate(' + (translation || 0) + ', ' + (translation || 0) + ') rotate(' + d.rotation + ')';
+        };
+    });
+}
+
 function drawCircle(container, circle) {
     container.append('path').attr({
         d: circle.d,
@@ -57,9 +88,21 @@ function drawArc(container, arc, start, end) {
     }).style(arc.style);
 }
 
+function rotate(selector, howMuch) {
+    var half = svg.width / 2;
+    var halfPx = half + 'px';
+    console.log('setting style');
+    document.querySelector(selector).style.setProperty('transform', 'rotate(' + howMuch % 361 + 'deg) translate(' + halfPx + ', ' + halfPx + ')');
+    document.querySelector(selector).style.setProperty('transform-origin', halfPx + ', ' + halfPx);
+
+    console.log(document.querySelector(selector).style.transform);
+}
+
 function outerIndicator(max) {
     var sliderWidth = 0.127;
-    var outerIndicator = svg.append("g").classed('outer-indicator', true);
+    var outerIndicator = svg.append("g").attr({
+    });
+    outerIndicator.classed('outer-indicator', true);
     var outerIndicatorArc =
         d3.svg.arc()
         .innerRadius(max - 2)
@@ -71,7 +114,7 @@ function outerIndicator(max) {
             .outerRadius(max)
             .startAngle(.1 * tau)
             .endAngle(.8 * tau),
-        transform: 'rotate(-30, 50, 50) translate(50,50)'
+        transform: rotateTranslate(-30)
     }).style({
         fill: 'rgba(255,255,255,0.1)'
     });
@@ -87,12 +130,10 @@ function outerIndicator(max) {
         })
         .attr({
             'd': outerIndicatorArc,
-            transform: 'rotate(-30, 50, 50) translate(50,50)'
+            transform: rotateTranslate(-30)
         });
 
-    setInterval(function() {
-        outerIndicatorSlider.transition().duration(750).call(arcTween, Math.max(.1, Math.min(Math.random(), .8 - sliderWidth)), sliderWidth, outerIndicatorArc);
-    }, 750);
+    return outerIndicator;
 }
 
 function outerSlider(max) {
@@ -100,8 +141,12 @@ function outerSlider(max) {
     var highlightStart = .11;
     var highlightEnd = .43;
 
-    var outerSlider = svg.append('g').classed('outer-slider', true);
-    var outerSliderHighlight = outerSlider.append('g').classed('outer-slider-highlight', true);
+    var outerSlider = svg.append('g').attr({
+        transform: transCenter()
+    });
+    outerSlider.classed('outer-slider', true);
+    var outerSliderHighlight = outerSlider.append('g');
+    outerSliderHighlight.classed('outer-slider-highlight', true);
 
     var outerSliderHighlightOuterBorderArc = arc(max, 1, {
         fill: 'rgba(255,255,255,0.4)'
@@ -136,7 +181,7 @@ function outerSlider(max) {
                                                 endAngle: highlightEnd * tau,
                                             }).attr({
                                                 d: arc.d,
-                                                transform: 'rotate(-30, 50, 50) translate(50,50)'
+                                                transform: rotateTranslate(-30)
                                             }).style(arc.style);
                                     });
 
@@ -147,14 +192,32 @@ function outerSlider(max) {
                 endAngle: (scale(i) + scale.rangeBand()) * tau
             }).attr({
                 d: arc.d,
-                transform: 'rotate(-30, 50, 50) translate(50,50)'
+                transform: rotateTranslate(-30)
             }).style(arc.style);
     });
+
+    var paddingArc = arc(max - 5.2, 0.7, outerSliderHighlightOuterBorderArc.style);
+
+    outerSliderHighlight.append('path')
+        .datum({
+            startAngle: -0.1 * tau,
+            endAngle: 0.1 * tau,
+        }).attr({
+            d: paddingArc.d
+        }).style(paddingArc.style);
+
+    outerSliderHighlight.append('path')
+        .datum({
+            startAngle: 0.4 * tau,
+            endAngle: 0.6 * tau,
+        }).attr({
+            d: paddingArc.d,
+        }).style(paddingArc.style);
 }
 
 function innerCoordinateCircle(max) {
     var circleElement = svg.append('g').attr({
-        transform: 'translate(50,50)'
+        transform: transCenter()
     });
 
     circleElement.classed('inner-coordinate-circle');
@@ -190,7 +253,7 @@ function innerCoordinateCircle(max) {
 
     var smallSegmentArcs = range(1, smallSegmentsCount).map(function() {
         return arc(max - 7, 1, {
-            fill: 'rgba(255,255,255,0.7)'
+            fill: 'rgba(255,255,255,0.4)'
         });
     });
 
@@ -242,9 +305,13 @@ function innerCoordinateCircle(max) {
 }
 
 function innerCircle(max) {
-    var container = svg.append('g').attr({
-        transform: 'translate(50,50)'
-    });
+    var container = svg.append('g')
+        .datum({
+            rotation: 0
+        })
+        .attr({
+            transform: transCenter()
+        });
 
     container.classed('inner-circle');
 
@@ -286,16 +353,22 @@ function innerCircle(max) {
         stroke: 'rgba(255,255,255,0.4)',
     });
 
+    // CROSSHAIR
+    var crosshairContainer = svg.append('g').datum({
+        rotation: 0
+    }).attr({
+        transform: transCenter()
+    });
     var innerCircle = circle(max - 19, 0.5, {
         fill: 'rgba(255,255,255,0.2)'
     });
 
-    container.append('path').attr({
+    crosshairContainer.append('path').attr({
         d: innerCircle.d,
     }).style(innerCircle.style);
 
-    // CROSSHAIR
-    container.append('line').attr({
+    crosshairContainer.classed('crosshair-container', true);
+    crosshairContainer.append('line').attr({
         x1: 0,
         x2: 0,
         y1: -1.5,
@@ -305,7 +378,7 @@ function innerCircle(max) {
         'stroke-width': 0.75,
     });
 
-    container.append('line').attr({
+    crosshairContainer.append('line').attr({
         x1: -1.5,
         x2: 1.5,
         y1: 0,
@@ -315,7 +388,7 @@ function innerCircle(max) {
         'stroke-width': 0.75,
     });
 
-    container.append('line').attr({
+    crosshairContainer.append('line').attr({
         y1: 4,
         y2: 5.5,
         x1: 0,
@@ -325,7 +398,7 @@ function innerCircle(max) {
         'stroke-width': 0.75,
     });
 
-    container.append('line').attr({
+    crosshairContainer.append('line').attr({
         y1: -5.5,
         y2: -4,
         x1: 0,
@@ -335,7 +408,7 @@ function innerCircle(max) {
         'stroke-width': 0.75,
     });
 
-    container.append('line').attr({
+    crosshairContainer.append('line').attr({
         x1: 4,
         x2: 5.5,
         y1: 0,
@@ -345,7 +418,7 @@ function innerCircle(max) {
         'stroke-width': 0.75,
     });
 
-    container.append('line').attr({
+    crosshairContainer.append('line').attr({
         x1: -5.5,
         x2: -4,
         y1: 0,
@@ -354,10 +427,21 @@ function innerCircle(max) {
         stroke: 'rgba(255,255,255,0.4)',
         'stroke-width': 0.75,
     });
+
+    setInterval(function() {
+        container.transition().duration(750).call(rotateTween, Math.random() * 30, viewBoxCenter);
+    }, 750);
+
+    return {
+        container, crosshairContainer
+    };
 }
 
 function pin(container, max, rotation) {
     var group = container.append('g');
+    group.attr({
+        transform: rotateTranslate(rotation)
+    });
     group.classed('pin', true);
 
     group.append('line').attr({
@@ -365,7 +449,6 @@ function pin(container, max, rotation) {
         y1: 0,
         x2: max - 2,
         y2: 0,
-        transform: 'translate(50,50) rotate(' + rotation + ')'
     }).style({
         stroke: 'rgba(255,255,255,0.2)',
         'stroke-width': 0.5
@@ -375,20 +458,34 @@ function pin(container, max, rotation) {
         cx: max - 1,
         cy: 0,
         r: 1,
-        transform: 'translate(50,50) rotate(' + rotation + ')'
     }).style({
         fill: 'rgba(255,255,255,0.2)',
     });
+
+    return group;
 }
 
 function guideCircle(container, max) {
-    container.append('circle').attr({
-        cx: 50,
-        cy: 50,
+    return container.append('circle').attr({
+        cx: viewBoxCenter,
+        cy: viewBoxCenter,
         r: max - 1
     }).style({
         fill: 'none',
         stroke: 'rgba(255,255,255,0.1)',
+        'stroke-width': 0.5
+    });
+}
+
+function guideLine(container, max, rotation) {
+    return container.append('line').attr({
+        x1: 15,
+        y1: 0,
+        x2: max - 2,
+        y2: 0,
+        transform: rotateTranslate(rotation)
+    }).style({
+        stroke: 'rgba(255,255,255,0.2)',
         'stroke-width': 0.5
     });
 }
@@ -402,3 +499,6 @@ innerCoordinateCircle(38);
 innerCircle(25);
 pin(svg, 50, 45);
 pin(svg, 50, 75);
+guideLine(svg, 60, 50);
+guideLine(svg, 60, 190);
+guideLine(svg, 60, 270);
