244d2baf5b54a827e3af35ef2ffcb21e53e24ae0
korneliusz <helluinster@gmail.com>
2016-01-22 23:44:42 +0100

    Complete the decision logic.

--- cljs/om/hello-world/src/hello_world/decision.cljs
+++ cljs/om/hello-world/src/hello_world/decision.cljs
@@ -4,8 +4,7 @@
             [om.dom :as dom :include-macros true]))
 
 ;; (defn downvote [decision])
-
-;; Component
+(defonce ENTER_KEY 13)
 
 (defn change [event decision owner]
   (om/set-state! owner :edit-text (-> event .-target .-value))
@@ -20,30 +19,52 @@
   (om/set-state! owner :score (+ 1 (om/get-state owner :score)))
   )
 
+(defn downvote-disabled [decision owner]
+  (<= (om/get-state owner :score) 0))
+
+(defn upvote-disabled [decision owner]
+  (>= (om/get-state owner :score) 10))
+
 (defn stars [owner]
   (dom/span nil (string/join (take (om/get-state owner :score) (repeat "☆"))))) 
 
+(defn is-editing [decision owner]
+  (== (om/get-state owner :is-editing) 1))
+
+(defn start-editing [event decision owner]
+  (om/set-state! owner :is-editing 1))
+
+(defn key-down [event decision owner]
+  (when (== (.-which event) ENTER_KEY)
+    (om/set-state! owner :is-editing 0)))
+
+;; Component
 
 (defn decision [decision owner]
   (reify
     om/IInitState
     (init-state [_]
       {:edit-text (:title decision)
-       :score 0})
+       :score 0
+       :is-editing 0})
 
     om/IRenderState
     (render-state [_ state]
       (dom/li #js {:className "decision"}
         (dom/div #js {:className "decision-view"}
-          (dom/input #js {:className "decision-name-edit"
-                          :value     (om/get-state owner :edit-text)
-                          :onChange  #(change % decision owner)})
+          (if
+              (is-editing decision owner)
+              (dom/input #js {:className "decision-name-edit"
+                              :value     (om/get-state owner :edit-text)
+                              :onChange  #(change % decision owner)
+                              :onKeyDown #(key-down % decision owner)})
+              (dom/label #js {:onDoubleClick #(start-editing % decision owner)} (om/get-state owner :edit-text))) ;; should probably show (:title decision), but don't yet know how to update that sensibly.
           (dom/button #js {:className "decision-upvote"
-                           :onClick #(upvote % decision owner)} "+")
+                           :onClick #(upvote % decision owner)
+                           :disabled (upvote-disabled decision owner)} "+")
           (dom/button #js {:className "decision-downvote"
-                           :onClick #(downvote % decision owner)} "-")
+                           :onClick #(downvote % decision owner)
+                           :disabled (downvote-disabled decision owner)} "-")
           (stars owner))))
-
     )
-
   )
